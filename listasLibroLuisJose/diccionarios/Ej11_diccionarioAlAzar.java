0b0ebe89fbbc6e8b61f17a82d3a227ec0b3fe7ba
pc <pc@pc-PC>
2020-03-18 14:02:46 +0100

    ss

--- listasLibroLuisJose/diccionarios/Ej11_diccionarioAlAzar.java
+++ listasLibroLuisJose/diccionarios/Ej11_diccionarioAlAzar.java
@@ -17,24 +17,38 @@ public class Ej11_diccionarioAlAzar {
 		larousse.put("papel_higienico", "toilet_paper");
 		larousse.put("prohibir", "forbid");
 // 		boolean distintos=false;
+		System.out.println(larousse.get("juego"));
 		NumeroAleatorio prueba= new NumeroAleatorio();
 		int[] mio= new int [2];
 		mio=prueba.getNumeros();
 		prueba.imprimeArray(mio);
 		System.out.println(mio[0]+" "+mio[1]+" "+mio[2]);
 		
-		System.out.println(larousse.values());
+		System.out.println(larousse.values()+ " los values"); 
 //		System.out.println(prueba.getNumeros());
 int correctas=0;
-		String [] a= larousse.keySet().toArray(new String[0]);
+		String [] a= larousse.keySet().toArray(new String[0]); //crea un array del tamaño necesario
+		//por cada entrada del key 
 		for( String al : a) {
 			System.out.print(al+" "); //las claves 
 		}
 		System.out.println(mio.length);
 		for (int i=0 ; i<mio.length; i++) {
-			System.out.println(a[mio[i]]+" : ");
+			System.out.println(a[mio[i]]+" : "); //el array a String le el valor de la posicion almacenada
+				// en el array mio; en cada pos tiene un valor distinto por el constructor de la clase
 			String palabrametida= e.next();
 			if(palabrametida.equals(larousse.get(a[mio[i]]))) {
+				/*
+			palabrametida es igual a del hashmap larousse, cojo con get te devuelve 
+			el valor de un value, pero necesito que le pases por parametro una clave / key
+			entonces, con el array String a en el que he metido las respuestas con un tamaño 
+			automatico gracias al metodo keysettoarraynewString[0]
+			tengo en cada una de las posiciones del array las respuestas ordenadas ...
+			si de ese array cojo la posicion que coincide con el valor que hay en el array mio posicion i;
+			estoy cogiendo de a el valor "0" que es forbid, o el valor "1" que es fever...
+			en definitiva el valor "i" que es cualquiera de los almacenados
+			
+			*/
 				System.out.println("palabra correcta");
 				correctas++; 
 			}else {
