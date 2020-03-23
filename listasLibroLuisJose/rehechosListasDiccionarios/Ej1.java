beff4c0007a833175f8c26c5989944237498a961
Javi <Javi@Javi-PC>
2020-03-23 17:06:41 +0100

    SIIII CONSEGUIDO LOS PATRONES EN ARRAYLIST

--- listasLibroLuisJose/rehechosListasDiccionarios/Ej1.java
+++ listasLibroLuisJose/rehechosListasDiccionarios/Ej1.java
@@ -9,16 +9,25 @@ public class Ej1 {
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
 		ArrayList<String> nombres = new ArrayList<String>();
+		nombres.add("Javier");
 		nombres.add("Moyano 1");
 		nombres.add("Corchero 2");
 		nombres.add("Tapetado");
-		nombres.add("Alberto");
-		nombres.add("Robert");
+		nombres.add("Alberto 3");
+		nombres.add("Robert 6");
+		nombres.add("Javier");
+		nombres.add("Moyano 2");
+		nombres.add("Corchero 3");
+		nombres.add("Tapetadoe");
+		nombres.add("Alberto 4");
+		nombres.add("Robert 6");
+		nombres.add("Javi");
+		nombres.add("Javier");
 		nombres.add("Javi");
 		nombres.add("e36");
 		for (String n : nombres) {
 			System.out.println(n);
-		}
+		}System.out.println("Primera vez impreso tras añadiro los nombres al arraylist");
 		for (int num = 0; num < nombres.size(); num++) {
 //			for (String n : nombres) {
 				nombres.removeIf(apellido -> apellido.equalsIgnoreCase("Javi"));
@@ -26,7 +35,7 @@ public class Ej1 {
 				Pattern vocales = Pattern.compile("[0-9]");
 //				Matcher m = vocales.matcher(String.valueOf(nombres.get(num).charAt(num)));
 //		nombres.removeIf(nomb -> nomb.contains(String.valueOf(nombres.get(num).charAt(num)))); 
-				nombres.removeIf(nombre-> nombre.contains("[0-9]"));
+//				nombres.removeIf(nombre-> nombre.contains("[0-9]")); => no funciona
 				nombres.removeIf(ape-> ape.equals("Tapetado"));
 //				nombres.removeIf(nomb -> nomb.contains(String.valueOf(n.charAt(num))));// si quiero eliminar por
 				
@@ -39,22 +48,46 @@ public class Ej1 {
 				 * ???
 				 */
 //		nombres.removeIf(nomb -> nomb.contains("[aeiou]"));
-			System.out.println(nombres);
-			
-			
-			String [] po= new String [4];
-			po[0]="Jose 1";
-			po[2]="juan";
-			po[1]="serco 2";
-			po[3]= "noe";
-			for (int i= 0; i< po.length; i++) {
-			Pattern numeros = Pattern.compile(".* [0-9]");
-			Matcher m  = numeros.matcher(po[i]);
-			 if (m.matches()) {
-				 System.out.println(po[i]);
-			 }
+//			System.out.println(nombres+" impreso como lista tras eliminar Tapetao");
+				
+				/////////////////AQUI LO CONSIGO METER EL PATRON Y CHECKEAR CONTRA EL PATRON///////////
+			Pattern numeros = Pattern.compile(".*[0-9]");
+			boolean borrado=false;
+			for (int i= 0; i< nombres.size(); i++) {
+				if(borrado==true) {
+					i=0;
+					borrado=false;
+				}
+				Matcher m  = numeros.matcher(nombres.get(i));
+				if(m.matches()) {
+//					System.out.println(nombres+" eliminando a "+ nombres.get(i));
+					nombres.remove(i);
+					
+					borrado=true;
+				}
+				
 			}
+			//////////////////////////////////******************
+//			System.out.println(nombres);
 			
-		}
+			
+		}System.out.println(nombres);
+		String [] po= new String [4];
+		po[0]="Jose 1";
+		po[2]="juan";
+		po[1]="serco 2";
+		po[3]= "noe";
+//		for (int i= 0; i< po.length; i++) {
+//		Pattern numeros = Pattern.compile(".* [0-9]");
+//		Matcher m  = numeros.matcher(po[i]);
+//		 if (m.matches()) {
+//			 System.out.println(po[i]);
+//			 po[i]="";
+//		 }for (String o: po) {
+//			 System.out.println(o);
+//		 }
+//		 
+//		}
+		
 	}
 }
