e671fe44e3ca2ac9bb9103ffaecdfca4a463e596
Javi <Javi@Javi-PC>
2020-04-19 23:24:08 +0200

    FIN

--- trivial/nuevisimo/Jugador.java
+++ trivial/nuevisimo/Jugador.java
@@ -7,47 +7,41 @@ import java.util.regex.Pattern;
 import heroquest.Dado;
 
 public class Jugador {
+	
+	//--------------------ATRIBUTOS--------------------------------------------
+	
 	private int casillaActual=0,movimiento,cantidadQuesitos=0;
 	static Pregunta prfinal= new Pregunta();
-	int acertadas=0;
+	private int acertadas=0;
 	private boolean quesitoAcciones=false,quesitoLugares=false, quesitoMusica=false,quesitoNombres=false;
 	String [] preguntasArray;
 	Scanner sc = new Scanner (System.in);
 	boolean ceroNoPuedeSalir=false,unoNoPuedeSalir=false,dosNoPuedeSalir=false,tresNoPuedeSalir=false;
 	 private String nombre="";
 	 boolean buena=false;
-	protected String getNombre() {
-	return nombre;
-}
-protected void setNombre(String nombre) {
-	boolean nombreCorrecto=false;
-	do {
-	try {
-			comparar(nombre);
-			nombreCorrecto=true;
-		} catch (ExcepcionNumerosenNombre e) {
-			// TODO Auto-generated catch block
-			//sc.next();
-			System.out.println("solo caracteres alfabeticos simples");
-			System.out.println("introduce un nuevo nombre");
-			nombre=sc.nextLine();
-		}
-	}while(!nombreCorrecto);
-	this.nombre = nombre;
-	nombreCorrecto=false;
-}
+	 
+///*******************************************************************************
+	 
+	 // --------------------------CONSTRUCTORES -------------------------------------------
+	 
 	public Jugador(String nombre) {
-		this.nombre=nombre;
-		
-		try {
-			comparar(nombre);
-		} catch (ExcepcionNumerosenNombre e) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
+			
+			this.nombre=nombre;
+			
+			try {
+				comparar(nombre);
+			} catch (ExcepcionNumerosenNombre e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+			}
 		}
-	}
-
 	
+	public Jugador() {
+		 
+	}
+	// -**********************************************************
+	
+
 	public void ganarQueso(boolean acierto, boolean quesito, String categoria) {
 		if(acierto && quesito) {
 			switch (categoria) {
@@ -97,12 +91,6 @@ protected void setNombre(String nombre) {
 		}
 	}
 	
-	public Jugador() {
-		 
-	}
-	protected int getCantidadQuesitos() {
-		return cantidadQuesitos;
-	}
 	public boolean moverse() { // devuelve true si el acierto es correcto
 		if(quesitoNombres && quesitoAcciones && quesitoLugares && quesitoMusica) {
 			System.out.println("fin de juego");
@@ -195,6 +183,7 @@ protected void setNombre(String nombre) {
 			System.out.println("error al parar el programa");
 		}
 	}
+	
 	public void imprimeCuantosQuesitosyPosicion() {
 		System.out.println("le toca al jugador "+this.getNombre()+ " estas en la casilla "+ this.getMovimiento());
 		if (this.getCantidadQuesitos()>1 || this.getCantidadQuesitos()==0) {
@@ -203,6 +192,64 @@ protected void setNombre(String nombre) {
 			System.out.println("tienes "+ this.getCantidadQuesitos()+ " quesito");
 		}
 	}
+	
+	boolean comparar(String nombre) throws ExcepcionNumerosenNombre{
+		nombre=nombre.toLowerCase();
+		Pattern pat = Pattern.compile("[abcdefghijklmnñopqrstuvwxyz ]+");
+		Matcher m= pat.matcher(nombre);
+		if(m.matches()) {
+			//System.out.println("el patron encaja");
+			buena=true;
+		}else {
+			
+			buena=false;
+			throw new ExcepcionNumerosenNombre();
+			
+			//break;
+		}
+
+return buena; 
+//////////////////////METODO CARLOS RUDIMENTARIO XD///////////////////////////////////////////
+
+//for (int i= 0 ; i< nombre.length(); i++) {
+//char carPos= nombre.charAt(i);
+//letra = letra.valueOf(carPos); 
+//valueOf(carPos);
+//if(abec.contains(letra)) {
+//System.out.println("esta posicion es una letra");
+//buena=true;
+//}else {
+//System.out.println("esta poscion NO ES LETRA");
+//buena=false;
+//}
+//}
+
+/////////////////////////////////////FIN METODO CARLOS///////////////////////////////////////////////////
+}
+
+	//-----------------------------GETTERS Y SETTERS------------------------
+	
+	protected String getNombre() {
+	return nombre;
+	}
+	protected void setNombre(String nombre) {
+		boolean nombreCorrecto=false;
+		do {
+		try {
+				comparar(nombre);
+				nombreCorrecto=true;
+			} catch (ExcepcionNumerosenNombre e) {
+				// TODO Auto-generated catch block
+				//sc.next();
+				System.out.println("solo caracteres alfabeticos simples");
+				System.out.println("introduce un nuevo nombre");
+				nombre=sc.nextLine();
+			}
+		}while(!nombreCorrecto);
+		this.nombre = nombre;
+		nombreCorrecto=false;
+	}
+
 	
 	protected int getCasillaActual() {
 		return casillaActual;
@@ -222,59 +269,31 @@ protected void setNombre(String nombre) {
 	protected void setAcertadas(int acertadas) {
 		this.acertadas = acertadas;
 	}
-	public void responder() {
-		PreguntaNombres ne = new PreguntaNombres();
-		HashMap<String, String> preguntado= ne.getTacoPreguntas();
-		 preguntasArray =preguntado.keySet().toArray(new String [0]);
-		Dado d6 = new Dado(preguntado.size());
-		String pregunta2 =preguntado.get(preguntasArray[d6.getValor()-1]);
-		System.out.println(preguntasArray[d6.getValor()-1]);
-		System.out.println("tu respuesta es: ");
-		
-		String respuestaJugador= sc.nextLine();
-		if (pregunta2.equalsIgnoreCase(respuestaJugador)) {
-			System.out.println("respuesta correcta!!!");
-		}else {
-			System.out.println("eres un analfabeto disney");
-		}
+	protected int getCantidadQuesitos() {
+		return cantidadQuesitos;
 	}
+	///******************************--------------------------------------------------------------------
 	
-	boolean comparar(String nombre) throws ExcepcionNumerosenNombre{
-		nombre=nombre.toLowerCase();
-		String letra="";
-		String abec= "abcdefghijklmnñopqrstuvwxyz";
-
-		//////////////////////METODO CARLOS RUDIMENTARIO XD///////////////////////////////////////////
-		
-		for (int i= 0 ; i< nombre.length(); i++) {
-//		 char carPos= nombre.charAt(i);
-//		  letra = letra.valueOf(carPos); 
-		  //valueOf(carPos);
-//		 if(abec.contains(letra)) {
-//			 System.out.println("esta posicion es una letra");
-//			 buena=true;
-//		 }else {
-//				System.out.println("esta poscion NO ES LETRA");
-//				buena=false;
-//			}
-		
-		/////////////////////////////////////FIN METODO CARLOS///////////////////////////////////////////////////
-			Pattern pat = Pattern.compile("[abcdefghijklmnñopqrstuvwxyz ]+");
-			Matcher m= pat.matcher(nombre);
-			if(m.matches()) {
-				//System.out.println("el patron encaja");
-				buena=true;
-			}else {
-				
-				buena=false;
-				throw new ExcepcionNumerosenNombre();
-				
-				//break;
-			}
 	
-
-		
-		
-		}return buena; 
-	}
+	
+	//--------------------CODIGO BASURA-------------------------------------------
+//	public void responder() {
+//	PreguntaNombres ne = new PreguntaNombres();
+//	HashMap<String, String> preguntado= ne.getTacoPreguntas();
+//	 preguntasArray =preguntado.keySet().toArray(new String [0]);
+//	Dado d6 = new Dado(preguntado.size());
+//	String pregunta2 =preguntado.get(preguntasArray[d6.getValor()-1]);
+//	System.out.println(preguntasArray[d6.getValor()-1]);
+//	System.out.println("tu respuesta es: ");
+//	
+//	String respuestaJugador= sc.nextLine();
+//	if (pregunta2.equalsIgnoreCase(respuestaJugador)) {
+//		System.out.println("respuesta correcta!!!");
+//	}else {
+//		System.out.println("eres un analfabeto disney");
+//	}
+//}
+	
+	
+	
 }
--- trivial/nuevisimo/Turno.java
+++ trivial/nuevisimo/Turno.java
@@ -19,25 +19,25 @@ public class Turno {
 	public Turno (Jugador jug) {
 		
 		
-		Jugador n2 = new Jugador("papa");
-		//Pregunta p = new Pregunta();
-		int o=0;
-		boolean finPartida=Jugador.prfinal.isFinPreguntasTodas();
-		while(!finPartida) { //mientras que la partida no se acaba 
-			System.out.println("le toca al jugador "+jug.getNombre()+ " estas en la casilla "+ jug.getMovimiento());
-			System.out.println("tienes "+ jug.getCantidadQuesitos()+ " quesitos");
-			while(jug.moverse()==true) { //moverse() devuelve un booleano en funcion de si la respuesta es correcta true, si es mala false
-				System.out.println("te toca otra vez  estas en la casilla "+ jug.getMovimiento());
-				System.out.println("tienes "+ jug.getCantidadQuesitos()+ " quesitos");
-			}
-			System.out.println("le toca al jugador "+n2.getNombre()+ " estas en la casilla "+ n2.getMovimiento());
-			while(n2.moverse()==true) {
-				System.out.println("te vuelve a tocar estas en la casilla "+ n2.getMovimiento());
-				System.out.println("tienes "+n2.getCantidadQuesitos()+ " quesitos");
-			}
-			System.out.println(Jugador.prfinal.getPrfalladas());
-			finPartida=Jugador.prfinal.isFinPreguntasTodas();
-		}
+//		Jugador n2 = new Jugador("papa");
+//		//Pregunta p = new Pregunta();
+//		int o=0;
+//		boolean finPartida=Jugador.prfinal.isFinPreguntasTodas();
+//		while(!finPartida) { //mientras que la partida no se acaba 
+//			System.out.println("le toca al jugador "+jug.getNombre()+ " estas en la casilla "+ jug.getMovimiento());
+//			System.out.println("tienes "+ jug.getCantidadQuesitos()+ " quesitos");
+//			while(jug.moverse()==true) { //moverse() devuelve un booleano en funcion de si la respuesta es correcta true, si es mala false
+//				System.out.println("te toca otra vez  estas en la casilla "+ jug.getMovimiento());
+//				System.out.println("tienes "+ jug.getCantidadQuesitos()+ " quesitos");
+//			}
+//			System.out.println("le toca al jugador "+n2.getNombre()+ " estas en la casilla "+ n2.getMovimiento());
+//			while(n2.moverse()==true) {
+//				System.out.println("te vuelve a tocar estas en la casilla "+ n2.getMovimiento());
+//				System.out.println("tienes "+n2.getCantidadQuesitos()+ " quesitos");
+//			}
+//			System.out.println(Jugador.prfinal.getPrfalladas());
+//			finPartida=Jugador.prfinal.isFinPreguntasTodas();
+//		}
 		
 		//System.out.println(p.devuelvePreguntasyRespuestasenObjetoPregunta("preguntasNombres"));
 //		System.out.println(p.devuelvePreguntasyRespuestasenObjetoPregunta("preguntasAcciones"));
@@ -49,31 +49,6 @@ public class Turno {
 	
 	public Turno () {
 		
-		Jugador n = new Jugador("javi");
-		Jugador n2 = new Jugador("papa");
-		//Pregunta p = new Pregunta();
-//		int o=0;
-//		boolean finPartida=Jugador.prfinal.isFinPreguntasTodas();
-//		while(!finPartida) {
-//			System.out.println("le toca al jugador "+n.getNombre()+ " estas en la casilla "+ n.getMovimiento());
-//			System.out.println("tienes "+ n.getCantidadQuesitos()+ " quesitos");
-//			while(n.moverse()==true) { //moverse() devuelve un booleano en funcion de si la respuesta es correcta true, si es mala false
-//				System.out.println("te toca otra vez  estas en la casilla "+ n.getMovimiento());
-//				System.out.println("tienes "+ n.getCantidadQuesitos()+ " quesitos");
-//			}
-//			System.out.println("le toca al jugador "+n2.getNombre()+ " estas en la casilla "+ n2.getMovimiento());
-//			while(n2.moverse()==true) {
-//				System.out.println("te vuelve a tocar estas en la casilla "+ n2.getMovimiento());
-//				System.out.println("tienes "+n2.getCantidadQuesitos()+ " quesitos");
-//			}
-//			System.out.println(Jugador.prfinal.getPrfalladas());
-//			finPartida=Jugador.prfinal.isFinPreguntasTodas();
-//		}
-		//System.out.println(p.devuelvePreguntasyRespuestasenObjetoPregunta("preguntasNombres"));
-//		System.out.println(p.devuelvePreguntasyRespuestasenObjetoPregunta("preguntasAcciones"));
-//		System.out.println(p.devuelvePreguntasyRespuestasenObjetoPregunta("preguntasLugares"));
-//		System.out.println(p.getPrfalladas());
-//		}
 	}
 	public static void main (String [] a) {
 		String nombre = JOptionPane.showInputDialog("Introduce tu nombre");
