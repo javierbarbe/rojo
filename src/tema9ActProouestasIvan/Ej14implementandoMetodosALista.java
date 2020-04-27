c4681ed7b6fd878638da32682c56abc593a715b2
Javi <Javi@Javi-PC>
2020-04-27 17:22:40 +0200

    14 resuelto?

--- src/tema9ActProouestasIvan/Ej14implementandoMetodosALista.java
+++ src/tema9ActProouestasIvan/Ej14implementandoMetodosALista.java
@@ -1,6 +1,7 @@
 package tema9ActProouestasIvan;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+
 
 public class Ej14implementandoMetodosALista {
 	/*
@@ -14,12 +15,17 @@ public class Ej14implementandoMetodosALista {
 		// TODO Auto-generated method stub
 		LinkedListeo jo = new LinkedListeo();
 		jo.insertarPrincipio("hola");
+		Persona niño = new Persona("mario", "valverde",2002);
 		jo.insertarPrincipio(new Persona ("Javi", "barbero", 1332));
+		jo.insertaFinal(niño);
 		
 		for(int i= 0; i<jo.listaCambia.size();i++) {
 			System.out.println(jo.listaCambia.get(i));
 		}
-		
+		jo.buscar("hola");
+		jo.buscar(new Persona ("Javi", "barbero", 1332));
+		jo.eliminarNodo(2);
+		jo.mostrarLista();
 	}
 
 	
@@ -29,14 +35,33 @@ public class Ej14implementandoMetodosALista {
 	
 }
 class LinkedListeo <T> {
-	T atri;
-	LinkedList<T> listaCambia= new LinkedList<>();
+	//T atri;
+	ArrayList<T> listaCambia= new ArrayList<>();
 	public void insertarPrincipio(T atri) {
 		listaCambia.add(0,atri);
 	}
 	
 	public void insertaFinal(T atri) {
 		listaCambia.add(atri);
+	}
+	
+	public void buscar(T objeto) {
+		if(listaCambia.contains(objeto)) {
+			System.out.println("el objeto existe");
+		}else {
+			System.out.println("no se encuentra");
+		}
+	}
+	public void mostrarLista() {
+		listaCambia.forEach((o)-> System.out.println(o));
+	}
+	
+	public void eliminarNodo(int posicion) {
+		if(posicion>=listaCambia.size()) {
+			System.out.println("te has pasado de tamaño de lista, no hay elementos en esa posicion");
+		}else {
+		listaCambia.remove(posicion);
+		}
 	}
 	
 }
--- src/tema9ActProouestasIvan/Persona.java
+++ src/tema9ActProouestasIvan/Persona.java
@@ -25,6 +25,38 @@ public class Persona {
 		this.añoNacimiento = añoNacimiento;
 	}
 	 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((apellidos == null) ? 0 : apellidos.hashCode());
+		result = prime * result + añoNacimiento;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		if (apellidos == null) {
+			if (other.apellidos != null)
+				return false;
+		} else if (!apellidos.equals(other.apellidos))
+			return false;
+		if (añoNacimiento != other.añoNacimiento)
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
 	public String toString() {
 		return this.nombre +" " + this.apellidos+ " "+ this.añoNacimiento;
 	}
